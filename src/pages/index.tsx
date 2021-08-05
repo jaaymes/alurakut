6882cf3dd043e1a0473c64d01fe6f38e36158a02
Jaymes Costa <jaaymes@gmail.com>
2021-08-05 11:58:34 -0300

    git branch desenvolvimento

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
+import { FormEvent } from "react";
 import { Box } from "../components/Box";
 import { MainGrid } from "../components/MainGrid";
 import { Menu } from "../components/Menu";
 import { OrkutNostalgicIconSet } from "../components/OrkutNostalgicIconSet";
 import { ProfileRelationsBoxWrapper } from "../components/ProfileRelationsBox";
+import { ProfileSideBarMenuDefault } from "../components/ProfileSideBarMenuDefault";
 
 type HomeProps = {
   gitHubUser: string;
 };
 
 function ProfileSideBar({ gitHubUser }: HomeProps) {
-  // console.log(gitHubUser)
   return (
     <Box>
       <img src={`https://github.com/${gitHubUser}.png`} alt="Jaymes Costa" />
+      <hr />
+      <a className="boxLink" href={`http://github.com/${gitHubUser}`}>
+        @{gitHubUser}
+      </a>
+      <hr />
+      <ProfileSideBarMenuDefault />
     </Box>
   );
 }
@@ -20,6 +27,12 @@ function ProfileSideBar({ gitHubUser }: HomeProps) {
 export default function Home() {
   const gitHubUser = "jaaymes";
   const pessoasFavoritas = ["luis", "jaaymes", "andre", "joao"];
+
+  function handleSubmit(e:FormEvent){
+    e.preventDefault();
+    console.log(e)
+
+  }
   return (
     <>
       <Menu gitHubUser={gitHubUser} />
@@ -31,6 +44,30 @@ export default function Home() {
           <Box>
             <h1 className="title">Bem vindo (a)</h1>
             <OrkutNostalgicIconSet />
+          </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <button>
+                Criar comunidade
+              </button>
+            </form>
           </Box>
         </div>
         <div style={{ gridArea: "profileRelationsArea" }}>
